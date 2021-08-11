5f9e2e62a461e310eaffbb315a849629d68e465e
Rdroshan <roshan.dawande@vogo.in>
2021-08-12 00:52:07 +0530

    :zap: updating variables.sh file with new env variables

--- variables.sh
+++ variables.sh
@@ -1,1 +1,7 @@
-export GOOGLE_API_KEYS=""
+export SECRET_KEY="django-insecure-!#65y5km3^9d600mqno6mpvpc*hht2s60h-^@q4ic9n1w*__+d"
+export GOOGLE_API_KEYS="AIzaSyCPB7ZlTO7rtfSz0wzZOeHkuhfOi9EzIwE"
+export DB_USER="videoapi_user"
+export DB_NAME="videoapi"
+export DB_PASS="1234"
+export DB_PORT=5432
+export DB_HOST="db"
